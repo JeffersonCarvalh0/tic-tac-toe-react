abc41138f842d40fb5d9109af27145d9fa1a2563
Jefferson <jeffersonhcarvalho@gmail.com>
2019-01-07 16:06:58 -0300

    Now it displays the location for each move

--- src/index.js
+++ src/index.js
@@ -72,7 +72,7 @@ class Game extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            history: [{ squares: Array(9).fill(null) },],
+            history: [{ squares: Array(9).fill(null), coord: { x : null, y: null } },],
             stepNumber: 0,
             xIsNext: true,
         };
@@ -82,11 +82,13 @@ class Game extends React.Component {
         const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const squares = current.squares.slice();
+        const last = { x : parseInt(i / 3) + 1, y: parseInt(i % 3) + 1 };
+
         if (calculateWinner(squares) || squares[i]) return;
 
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
-            history: [...history, { squares: squares }],
+            history: [...history, { squares: squares, coord: last, }],
             stepNumber: history.length,
             xIsNext: !this.state.xIsNext,
         });
@@ -105,7 +107,7 @@ class Game extends React.Component {
         const winner = calculateWinner(current.squares);
 
         const moves = history.map((step, move) => {
-            const desc = move ? 'Go to move #' + move : 'Go to game start';
+            const desc = move ? `Go to move #${move} (${history[move].coord.x}, ${history[move].coord.y})` : 'Go to game start';
             return (
                 <li key={ move }>
                     <button onClick={ () => this.jumpTo(move) }>{ desc }</button>
