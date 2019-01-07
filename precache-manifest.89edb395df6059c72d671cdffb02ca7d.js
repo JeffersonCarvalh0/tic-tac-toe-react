89b93e4677996e5c7bb16c1cbf28fe113de0255d
Jefferson <jeffersonhcarvalho@gmail.com>
2019-01-07 13:20:34 -0300

    Updates

---
+++ precache-manifest.89edb395df6059c72d671cdffb02ca7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "24a1bc39042ecf940668",
+    "url": "/tic-tac-toe-react/static/js/runtime~main.24a1bc39.js"
+  },
+  {
+    "revision": "9088fa2569909f1b0233",
+    "url": "/tic-tac-toe-react/static/js/main.9088fa25.chunk.js"
+  },
+  {
+    "revision": "225ee6f639194e355b59",
+    "url": "/tic-tac-toe-react/static/js/1.225ee6f6.chunk.js"
+  },
+  {
+    "revision": "9088fa2569909f1b0233",
+    "url": "/tic-tac-toe-react/static/css/main.7d0960b6.chunk.css"
+  },
+  {
+    "revision": "98cf6ef28861e8c28868fc71bb572bbd",
+    "url": "/tic-tac-toe-react/index.html"
+  }
+];
